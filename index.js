fe471344b0c788bc9eee095979ac4aa406ec2caa
bharatkhanna29 <bharat@programmingpathshala.com>
2023-06-09 14:48:40 +0530

    Add Numbers API

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+function addNums(req, res) {
+    if (req.query.a == null || req.query.b == null) {
+        res.status(400).send({error: "Numbers are given as null"})
+    }
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    const name = req.query.name;
+    console.log(name);
+    const sum = a + b;
+    res.status(200).send(
+        {
+            "sum": sum
+        }
+    );
+}
+
+app.patch('/addTwoNums', addNums)
+
+const port = 3001
+const ip = "localhost"
+app.listen(port, ip, function() {
+    console.log("I am listening");
+})
